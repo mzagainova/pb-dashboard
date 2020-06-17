ec5f50bc29039c2da6fe3e0a427a8dc4788f0ac9
mariya.zagainova.00110619 <mariya.zagainova@jhuapl.edu>
2020-06-17 14:15:39 -0400

    turned bubble heatmap into react component and added to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BrutalityByState from "../src/components/widgets/BrutalityByState";
 import BrutalityOverTime from "../src/components/widgets/BrutalityOverTime";
+import BubbleHeatmap from "../src/components/widgets/BubbleHeatmap"
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <h1>Police Brutality Dashboard</h1>
       <BrutalityOverTime />
       <BrutalityByState />
+      <BubbleHeatmap />
     </div>
   );
 }
